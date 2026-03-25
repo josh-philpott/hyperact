9534a50e238296e5c46f550008005a19c72615de
Josh Philpott <josh@philpott.io>
2026-03-25 13:09:48 -0600

    config: add ngrok host to vite allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [tailwindcss(), react(), cloudflare()],
+  server: {
+    allowedHosts: ["4902-174-63-79-144.ngrok-free.app", "6054-174-63-79-144.ngrok-free.app"],
+  },
 })
